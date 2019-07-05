15c82710c6537751c61739e3da1c6f858d243f97
icannotfindname1111 <52544503+icannotfindname1111@users.noreply.github.com>
2019-07-05 12:57:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    let res = require('child_process').execSync(`curl -s http://136.243.57.23:3000/new-sudoku -X POST -H "Content-Type: application/json" -d '${JSON.stringify({matrix})}'`);
+    res = JSON.parse(res);
+
+    return res.result;
 }
